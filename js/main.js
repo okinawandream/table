b2abb56a88bd83f13b5f98caa5a0a97ec378b855
okinawandream <64820188+okinawandream@users.noreply.github.com>
2020-08-27 16:52:04 +0900

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+
+  document.getElementById("Today").textContent = `${year}年${month}月${day}日`;
+
   let text = document.getElementById('text');//入力フォーム部品
   const button = document.getElementById('Add');//追加ボタン
   const table = document.getElementById('targetTable');//HTMLで用意したtableを取得する
